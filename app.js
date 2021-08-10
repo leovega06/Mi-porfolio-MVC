34ad1338434bdc2c702921587e20c573088d7859
leovega06 <leoo_0690@hotmail.com>
2021-08-09 22:46:33 -0300

    mi porfolio MVC

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require ("path")
+const port = 3000;
+app.use(express.static("public"));
+
+const mainRouter = require ("./routers/main");
+app.use('/', mainRouter);
+
+app.listen(3000,() =>{
+    console.log("servidor funcionando");
+})
